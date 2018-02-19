cdf169de9236958fff42106a14a5470575cb034f
Franklin Wang <20156676+bigbonustoday@users.noreply.github.com>
2018-02-19 13:25:16 -0500

    add em algo example for mixture gaussian

--- em_algorithm.py
+++ em_algorithm.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 theta0 = {
-    'mu1': 0,
-    'mu2': 1,
-    'sigma1': 1,
-    'sigma2': 1,
+    'mu1': 1,
+    'mu2': 8,
+    'sigma1': 5,
+    'sigma2': 5,
     'p': 0.5
 }
 
@@ -14,30 +14,70 @@ theta1 = {
     'mu2': 6,
     'sigma1': 2,
     'sigma2': 4,
-    'p': 0.3
+    'p': 0.9
 }
 
-def simulate_mixed_normal(size, theta):
+
+def simulate_mixture_gaussian(size, theta):
     x1 = np.random.normal(loc=theta['mu1'], scale=theta['sigma1'], size=size)
-    x2 = np.random.normal(loc=theta['m2'], scale=theta['sigma2'], size=size)
+    x2 = np.random.normal(loc=theta['mu2'], scale=theta['sigma2'], size=size)
     w = np.random.binomial(n=1, p=theta['p'], size=size)
-    return x1 * w + x2 * (1 - w)
+    x = x1 * w + x2 * (1 - w)
+    return x
+
 
 def _log_likelihood_mixed_normal(x, theta):
-    p1 = 1 / (2 * np.pi * theta['sigma1'] ** 2) * np.exp(- 1.0 / 2 / theta['sigma1'] ** 2 * (x - theta['mu1']) ** 2)
-    p2 = 1 / (2 * np.pi * theta['sigma2'] ** 2) * np.exp(- 1.0 / 2 / theta['sigma2'] ** 2 * (x - theta['mu2']) ** 2)
-    l = np.log(p1) * theta['p'] + np.log(p2) * (1 - theta['p'])
+    p1 = 1.0 / (2 * np.pi * theta['sigma1'] ** 2) ** 0.5 * np.exp(
+        - 1.0 / 2 / theta['sigma1'] ** 2 * (x - theta['mu1']) ** 2)
+    p2 = 1.0 / (2 * np.pi * theta['sigma2'] ** 2) ** 0.5 * np.exp(
+        - 1.0 / 2 / theta['sigma2'] ** 2 * (x - theta['mu2']) ** 2)
+    l = np.sum(np.log(p1 * theta['p'] + p2 * (1 - theta['p'])))
 
     # find posterior distribution of Z
     prob_z = p1 * theta['p'] / (p1 * theta['p'] + p2 * (1 - theta['p']))
-    return {'logL': np.sum(l),
-    'prob_z': prob_z}
+    return {'logL': l,
+            'prob_z': prob_z}
 
 
 # use EM algorithm to fit mixed normal distribution
-def fit_mixed_normal(x, theta0=theta0, max_attempt=10):
+def fit_mixture_gaussian(x, theta0=theta0, max_attempt=10):
     theta = theta0
+
+    print('Attempt #0')
+    l = _log_likelihood_mixed_normal(x, theta)['logL']
+    print(list(theta.values()))
+    print('LogL = ' + str(l))
+
     for attempt in range(max_attempt):
-        print 'Attempt #', attempt + 1
-        print  '...', theta.values()
+        print('Attempt #' + str(attempt + 1))
+
+        # E step
         prob_z = _log_likelihood_mixed_normal(x, theta)['prob_z']
+
+        # M step
+        prob_z_1 = 1 - prob_z
+        mu1 = np.sum(prob_z * x) / np.sum(prob_z)
+        mu2 = np.sum(prob_z_1 * x) / np.sum(prob_z_1)
+        sigma1 = (np.sum(prob_z * (x - mu1) ** 2) / np.sum(prob_z)) ** 0.5
+        sigma2 = (np.sum(prob_z_1 * (x - mu2) ** 2) / np.sum(prob_z_1)) ** 0.5
+        p = np.mean(prob_z)
+
+        # update
+        theta = {
+            'mu1': mu1,
+            'mu2': mu2,
+            'sigma1': sigma1,
+            'sigma2': sigma2,
+            'p': p
+        }
+        # log likelihood should increase at each iteration!!
+        l = _log_likelihood_mixed_normal(x, theta)['logL']
+        print(list(theta.values()))
+        print('LogL = ' + str(l))
+    return theta
+
+
+def main():
+    x = simulate_mixture_gaussian(size=1000000, theta=theta1)
+    theta_fittd = fit_mixture_gaussian(x, max_attempt=100)
+    return
