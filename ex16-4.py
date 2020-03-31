ebede8d6a7522ed733de9241a1dcf267cbe6f342
bigbonustoday <franklinwangqiong@gmail.com>
2020-03-31 12:55:19 -0400

    minor changes

--- ex16-4.py
+++ ex16-4.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.linear_model import ElasticNet, LogisticRegression, Lasso, Ridge
+from sklearn.linear_model import LogisticRegression, Lasso, Ridge
 
 
 def main():
